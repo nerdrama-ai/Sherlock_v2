eb149ad56809d2a6f800b4b67c06d9af50f1c767
nerdrama.ai <nerdrama.ai@gmail.com>
2025-09-03 00:57:53 +0530

    Delete frontend directory

--- frontend/lib/api.js
+++
@@ -1,7 +0,0 @@
-export async function fetchSherlock(username) {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/search?username=${username}`
-  );
-  if (!res.ok) throw new Error("Failed to fetch results");
-  return res.json();
-}
